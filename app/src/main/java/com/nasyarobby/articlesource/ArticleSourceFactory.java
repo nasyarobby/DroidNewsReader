60b1fe6ece39640daccb3497e30f5ecabdfb891f
Nasyarobby <nasyarobby@gmail.com>
2018-12-06 09:31:28 -0500

    Added a factory class to generate new source

---
+++ app/src/main/java/com/nasyarobby/articlesource/ArticleSourceFactory.java
@@ -0,0 +1,39 @@
+package com.nasyarobby.articlesource;
+
+import com.nasyarobby.articlesource.newsapiorg.Newsapiorg;
+import com.nasyarobby.articlesource.newsapiorg.NewsapiorgSourceAllHeadlines;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleSourceFactory {
+    private Newsapiorg newsapi;
+    private List<String> sources;
+    public ArticleSourceFactory() {
+        newsapi = new Newsapiorg("1dfd051041da4379987904e6b77c42d5");
+
+        sources = new ArrayList<>();
+        sources.add("All Headlines");
+        sources.add("BBC News");
+        sources.add("CNN News");
+    }
+
+    public ArticleSource getSource(String name) {
+        newsapi.reset();
+
+        ArticleSource source = null;
+        if(name.equals("All Headlines")) {
+            source = new NewsapiorgSourceAllHeadlines(newsapi);
+        }
+        else if(name.equals("BBC News")) {
+            source = new BbcNewsSource(newsapi);
+        } else if(name.equals("CNN News")){
+            source = new CnnSource(newsapi);
+        }
+        return source;
+    }
+
+    public List<String> getSourceNames() {
+        return sources;
+    }
+}
