c003d80a6a910d3f63ba988c0711d6e37935e5db
Nasyarobby <nasyarobby@gmail.com>
2018-12-06 10:20:15 -0500

    remove unnecessary casting

--- app/src/main/java/com/nasyarobby/droidnewsreader/MainActivity.java
+++ app/src/main/java/com/nasyarobby/droidnewsreader/MainActivity.java
@@ -70,10 +70,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,13 +82,13 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         Menu menu = navigationView.getMenu();
         Menu headlinesMenu = menu.addSubMenu("Headlines");
         for (int i = 0; i < factory.getSourceNames().size(); i++) {
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity
 
         navigationView.setNavigationItemSelectedListener(this);
 
-        RecyclerView articleRecyclerView = (RecyclerView) findViewById(R.id.articles_recyler_view);
+        RecyclerView articleRecyclerView = findViewById(R.id.articles_recyler_view);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         articleRecyclerView.setLayoutManager(layoutManager);
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
